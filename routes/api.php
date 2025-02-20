80e6c54fedde58792a2bad799b8847cb7640f025
Matheus Taffe <matheustaffe@hotmail.com>
2025-02-20 14:54:22 -0300

    teste 2

---
+++ routes/api.php
@@ -0,0 +1,11 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ImageController;
+
+Route::get('/image', [ImageController::class, 'genareteImage']);
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
