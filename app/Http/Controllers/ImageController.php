1b06309b363ead660f190f3dcb4d1a853a9a374d
Matheus Taffe <matheustaffe@hotmail.com>
2025-02-23 01:06:16 -0300

    Funcional

--- app/Http/Controllers/ImageController.php
+++ app/Http/Controllers/ImageController.php
@@ -8,23 +8,52 @@ use Intervention\Image\Drivers\Gd\Driver;
 
 class ImageController extends Controller
 {
-    public function genareteImage(Request $request)
+    public function generateImage(Request $request)
     {
-        $percent = 33;
+        // Validação da requisição
+        $request->validate([
+            'file' => 'required|image|mimes:jpeg,png,jpg|max:2048',
+            'percent' => 'required|integer|min:1|max:100',
+        ]);
+
+        // Pegando a imagem e a porcentagem do request
+        $percent = (int) $request->input('percent');
+        $imageFile = $request->file('file');
+
+        // Criando instância do ImageManager
         $imageManager = new ImageManager(new Driver());
-        $croppedImage = $imageManager->read('images/rex1.png');
-        $linePosition = $croppedImage->height() * ($percent / 100);
-        $croppedImage = $croppedImage->crop($croppedImage->width(), $linePosition)->save('images/rex2-top.png');
 
-        $blurImage = $imageManager->read('images/rex1.png')
-        ->blur(3)
-        ->greyscale()
-        ->pixelate(3)
-        ->blur(5)
-        ->save('images/rex3-blur2.png');
-        $blurImage->place('images/rex2-top.png', 'top', 0, 40, 100);
-        $blurImage->save('images/rex3-blur.png');
+        // Lendo a imagem recebida
+        $originalImage = $imageManager->read($imageFile->getPathname());
 
-        return response()->file('images/rex3-blur.png');
+        // Calculando a posição do corte
+        $linePosition = (int) ($originalImage->height() * ($percent / 100));
+
+        // Criando a parte superior cortada
+        $croppedImage = $originalImage->crop($originalImage->width(), $linePosition);
+        $croppedPath = storage_path('app/public/rex-top.png');
+        $croppedImage->save($croppedPath);
+
+        // Criando a imagem desfocada
+        $blurImage = $imageManager->read($imageFile->getPathname()) //$originalImage->blur(3)
+            ->blur(3)
+            ->greyscale()
+            ->pixelate(3);
+
+        // Salvando a imagem desfocada
+        $blurPath = storage_path('app/public/rex-blur.png');
+        $blurImage->save($blurPath);
+
+        // Colocando a parte cortada sobre a imagem desfocada
+
+        $finalImage = $imageManager->read($blurPath);
+        $finalImage->place($croppedPath, 'top', 0, 20, 100);
+
+        // Salvando a imagem final
+        $finalPath = storage_path('app/public/rex3-blur.png');
+        $finalImage->save($finalPath);
+
+        // Retornando a imagem editada
+        return response()->download($finalPath)->deleteFileAfterSend(true);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -4,8 +4,4 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ImageController;
 
-Route::get('/image', [ImageController::class, 'genareteImage']);
-
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::post('/image', [ImageController::class, 'generateImage']);
